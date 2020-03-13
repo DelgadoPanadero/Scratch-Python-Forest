5e1e710df5e9e373b8722ee9a9ef6c7d171c2f94
Ángel Delgado <angel.sherpa80@gmail.com>
2020-03-13 02:08:30 +0100

    chore(): add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='BonsaiGarden',
+      version='0.0',
+      description='The funniest joke in the world',
+      url='https://github.com/DelgadoPanadero/Scratch-Python-Forest.git',
+      author='Angel Delgado',
+      author_email='delgadopanadero@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          "joblib==0.14.1",
+          "numpy==1.18.1",
+          "scipy==1.4.1",
+          "sklearn==0.0",
+          "scikit-learn==0.22.1"
+      ],
+)

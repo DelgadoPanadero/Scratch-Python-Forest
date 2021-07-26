ad22c93c80faea4e49d00759f22e860b8961c6a6
DelgadoPanadero <angel.sherpa80@gmail.com>
2021-07-26 19:01:27 +0200

    fix(#23): name typo

--- skratch/garden/__init__.py
+++ skratch/garden/__init__.py
@@ -1,2 +1,2 @@
 from .random_garden_classifier import RandomGardenClassifier
-from .boosting_garden_classifier import BoostingGardenClassifer
+from .boosting_garden_classifier import BoostingGardenClassifier
